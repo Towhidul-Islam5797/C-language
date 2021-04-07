bcad96d76d7ee70c24e5fd02a7683b49d5941f09
towhidul-islam5797 <81187612+Towhidul-Islam5797@users.noreply.github.com>
2021-04-08 01:17:45 +0600

    Update Experiment3.c

--- Experiment3.c
+++ Experiment3.c
@@ -3,19 +3,26 @@ void power(int a, int b);
 int result = 1;
 int main()
 {
-    int x = 2 , y = 8, z;
-    power(x , y);
-    printf("The final value is :%d\n", result);
+    int x , y ;
+
+    printf("Enter the initial value : "); // it will get the initial value of the series
+    scanf("%d",&x);
+
+    printf("Enter the secondary value : "); // it is max number of the power for initial value
+    scanf("%d",&y);
+
+    power(x , y); // the UDF function
+    printf("The final value is :%d\n"); // it will bring the result
 
 return 0;
 }
 
-void power(int a, int b)
+void power(int a, int b) // this is the UDF function 
 {
     int i;
-    for(i= 1 ; i<=b ; i++)
+    for(i= 1 ; i<=b ; i++) // loop for the defining times of running 
         {
-            result *=  a;
+            result *=  a; //it actually brings the result of power 
             printf("%d\n",result);
         }
 }
