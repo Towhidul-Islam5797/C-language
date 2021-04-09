74bec737981cef0335a4a788b8759c59d04a3b6f
towhidul-islam5797 <81187612+Towhidul-Islam5797@users.noreply.github.com>
2021-04-09 16:57:07 +0600

    Add files via upload

---
+++ Experiment5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+//void pyramid(int n);
+int main()
+{
+    int n ,y;
+    printf("Enter the value of N:");
+    scanf("%d",&n);
+
+     y = pyramid(n);
+
+}
+void pyramid(int n)
+ //n(7) is the count of “*” in final row
+{
+    int i, j ;
+
+    for(i = 1 ; i <= n; i += 2)   //Loop from one row to another row
+        {
+            for(j=1; j<=(n-i)/2; j++)
+                {
+            printf(" ");
+                }
+            for(j = 1 ; j <= i ; j++) //Loop inside each row
+                {
+                    printf("*");
+                }
+            printf("\n");
+        }
+
+}
