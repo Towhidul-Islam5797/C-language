d55397fb7ab9ffd9018abe122f98ad2111eabc28
towhidul-islam5797 <81187612+Towhidul-Islam5797@users.noreply.github.com>
2021-04-12 02:20:12 +0600

    Update By UDF check prime not prime.c

--- By UDF check prime not prime.c
+++ By UDF check prime not prime.c
@@ -1,33 +1,59 @@
 #include<stdio.h>
-#include<math.h>
 
 int chk_prime(int x); //declaring a UDF for checking prime non-prime
 
 int main()
 {
-    int a, z;
-    printf("Enter a number :");
+    int a;
+    printf("Enter a number : ");
     scanf("%d" , &a);
 
-    z = chk_prime(a);
+    int z = chk_prime(a);
+
+    if(z == 0)
+        {
+        printf("\n%d is not a prime  number", a);
+    }
+    else{
+        printf("\n%d is a prime number", a);
+    }
 
 return 0;
 }
 int chk_prime(int x)
 {
-      int i, m;
-      for(i = 2; i <= x/2; i ++){
+    int i, m;
 
-        if(x%i == 0){
-            m = 0;
-            break;
+    if(x == 0)
+    {
+        m = 0;
+        return m;
+    }
+    if(x == 1)
+    {
+        m = 0;
+        return m;
+    }
+    if(x == 2)
+    {
+        m = 1;
+        return m;
+    }
+    if(x %2 == 0)
+    {
+        m = 0;
+        return m;
+    }
+    else
+        {
+        for(i = 3 ; i <= x/2 ; i += 2)
+        {
+                if(x%i == 0)
+                {
+                    m = 0;
+                    break;
+                }
         }
     }
-    if(m == 0){
-         printf("\n%d is a composit number", x);
-    }
-    else{
-         printf("\n%d is a prime number", x);
-    }
-return x;
+return m;
 }
