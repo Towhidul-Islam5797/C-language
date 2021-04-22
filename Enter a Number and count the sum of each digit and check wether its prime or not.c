048f5e7368da34e5ba7f6ec4442cee4a6a87bf79
towhidul-islam5797 <81187612+Towhidul-Islam5797@users.noreply.github.com>
2021-04-22 11:09:23 +0600

    Update and rename Problem3.cpp to Enter a Number and count the sum of each digit and check wether its prime or not.c

--- Enter a Number and count the sum of each digit and check wether its prime or not.c
+++ Enter a Number and count the sum of each digit and check wether its prime or not.c
@@ -6,14 +6,11 @@ int main()
     printf("Enter a number :");
     scanf("%d", &n);
 
-    while( n != 0)
-    {
-        m = m * 10;
-        m = m + n%10;
-        n = n / 10;
-    }
-    printf("Reverse of the number = %d", m);
+    while(n != 0){
 
+        m = m + n%10;
+        n = n /10;
+    }
     for(i = 2; i <= m/2; i ++){
 
         if(m%i == 0){
@@ -22,7 +19,7 @@ int main()
         }
     }
     if(x == 0){
-         printf("\n%d is a composit number", m);
+         printf("\n%d is a compsit number", m);
     }
     else{
          printf("\n%d is a prime number", m);
