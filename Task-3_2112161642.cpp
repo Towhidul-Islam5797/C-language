5af94b5227dfcc132d94373b48a7824e82ce8546
towhidul-islam5797 <81187612+Towhidul-Islam5797@users.noreply.github.com>
2021-04-29 12:28:49 +0600

    Add files via upload

---
+++ Task-3_2112161642.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int n  , i , space , col;
+
+    printf("Enter the number of Rows : "); //prompting user for a number he desires
+    scanf("%d", &n);
+
+    for(i = 0 ; i <= n ; i++) //parent loop (run loop) till the number of rows
+    {
+        for(col = 1 ; col <= n-i && col < n ; col++ )
+        {
+            printf(" %d " , col);
+        }
+        if(i == 0)
+        {
+            printf(" %d " , col);
+        }
+        else
+        {
+            for(space = 1 ; space < (6*i) - 2 ; space ++)
+            {
+                if( i == 0)
+                {
+                    continue; //i == 0 ; then space is not needed
+                }
+                printf(" ");
+            }
+        }
+
+        for(col = n-i ; col >= 1 ; col--)
+        {
+            if(col == n) // if col = n ; number is not needed to print
+            {
+                continue;
+            }
+            printf(" %d ", col);
+        }
+        printf("\n");
+    }
+}
