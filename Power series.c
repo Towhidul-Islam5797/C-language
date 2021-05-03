60eb172987da557dc461b7c5647a89a66977f7f7
towhidul-islam5797 <81187612+Towhidul-Islam5797@users.noreply.github.com>
2021-05-03 12:28:46 +0600

    Update and rename Experiment3.c to Power series.c

---
+++ Power series.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+void power(int a, int b);
+int result = 1;
+int main()
+{
+    int x , y ;
+
+    printf("Enter the initial value : "); // it will get the initial value of the series
+    scanf("%d",&x);
+
+    printf("Enter the secondary value : "); // it is max number of the power for initial value
+    scanf("%d",&y);
+
+    power(x , y); // the UDF function
+    printf("The final value is :%d\n"); // it will bring the result
+
+return 0;
+}
+
+void power(int a, int b) // this is the UDF function 
+{
+    int i;
+    for(i= 1 ; i<=b ; i++) // loop for the defining times of running 
+        {
+            result *=  a; //it actually brings the result of power#include <stdio.h>
+void power(int a, int b);
+int result = 1;
+int main()
+{
+    int x , y ;
+
+    printf("Enter the initial value : "); // it will get the initial value of the series
+    scanf("%d",&x);
+
+    printf("Enter the secondary value : "); // it is max number of the power for initial value
+    scanf("%d",&y);
+
+    power(x , y); // the UDF function
+    printf("The final value is : %d\n" , result); // it will bring the result
+
+return 0;
+}
+
+void power(int a, int b) // this is the UDF function
+{
+    int i;
+    for(i= 1 ; i<=b ; i++) // loop for the defining times of running
+        {
+            result *=  a; //it actually brings the result of power
+            printf(" %d\n",result);
+        }
+}
+
+        }
+}
